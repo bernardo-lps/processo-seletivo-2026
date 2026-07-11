247e0d00bcf186bbd0dfbfcd4b18ebfa6c9384c4
bernardo-lps <bernardolopesmachado15@gmail.com>
2026-07-11 11:28:22 -0300

    primeiro commit

---
+++ bernardo/src/main/java/com/lapes/bernardo/BernardoApplication.java
@@ -0,0 +1,13 @@
+package com.lapes.bernardo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BernardoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BernardoApplication.class, args);
+	}
+
+}
---
+++ bernardo/src/test/java/com/lapes/bernardo/BernardoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lapes.bernardo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BernardoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
